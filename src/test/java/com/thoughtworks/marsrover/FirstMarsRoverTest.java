7da0787d0393ccad9e9c346d2648e714b897b63f
Huangyicai <yicai.huang@thoughtworks.com>
2020-01-13 15:04:05 +0800

    [Add] enhance MarsRover turn function.

--- src/test/java/com/thoughtworks/marsrover/FirstMarsRoverTest.java
+++ src/test/java/com/thoughtworks/marsrover/FirstMarsRoverTest.java
@@ -2,6 +2,7 @@ package com.thoughtworks.marsrover;
 
 import static com.thoughtworks.marsrover.Direction.EAST;
 import static com.thoughtworks.marsrover.Direction.NORTH;
+import static com.thoughtworks.marsrover.Direction.SOUTH;
 import static com.thoughtworks.marsrover.Direction.WEST;
 import static org.junit.Assert.assertEquals;
 
@@ -11,29 +12,42 @@ import org.junit.Test;
 public class FirstMarsRoverTest {
 
   @Test
-  public void return_X5_Y6_NORTH_for_init_MarsRover() {
+  public void return_X5_Y6_NORTH_for_init_MarsRover_X5_Y6() {
     MarsRover marsRover = new MarsRover(5, 6, NORTH);
     assertEquals(marsRover.getPos(), Position.of(5, 6, NORTH));
   }
 
   @Test
-  public void return_X6_Y6_NORTH_for_MarsRover_move_single_step() {
+  public void return_X6_Y6_NORTH_for_MarsRover_at_X5_Y6_move_single_step() {
     MarsRover marsRover = new MarsRover(5, 6, NORTH);
     marsRover.moveForward();
     assertEquals(marsRover.getPos(), Position.of(6, 6, NORTH));
   }
 
   @Test
-  public void return_X5_Y6_WEST_for_MarsRover_turn_left_1_time() {
+  public void return_X5_Y6_WEST_for_MarsRover_forward_NORTH_turn_left_1_time() {
     MarsRover marsRover = new MarsRover(5, 6, NORTH);
     marsRover.turnLeft();
     assertEquals(marsRover.getPos(), Position.of(5, 6, WEST));
   }
 
   @Test
-  public void return_X5_Y6_EAST_for_MarsRover_turn_right_1_time() {
+  public void return_X5_Y6_EAST_for_MarsRover_forward_NORTH_turn_right_1_time() {
     MarsRover rover = new MarsRover(5, 6, NORTH);
     rover.turnRight();
     assertEquals(rover.getPos(), Position.of(5, 6, EAST));
+  }
+
+  @Test
+  public void return_X5_Y6_NORTH_for_MarsRover_forward_EAST_turn_left_1_time() {
+    MarsRover marsRover = new MarsRover(5, 6, EAST);
+    marsRover.turnLeft();
+    assertEquals(marsRover.getPos(), Position.of(5, 6, NORTH));
+  }
+  @Test
+  public void return_X5_Y6_WEST_for_MarsRover_forward_SOUTH_turn_left_1_time() {
+    MarsRover marsRover = new MarsRover(5, 6, SOUTH);
+    marsRover.turnRight();
+    assertEquals(marsRover.getPos(), Position.of(5, 6, WEST));
   }
 }
