449aae4b040b51ba1c028e8dc03c9e143fa1aae3
Huangyicai <yicai.huang@thoughtworks.com>
2020-01-15 23:38:01 +0800

    [Add] add commander for turn direction right.

--- src/test/java/com/thoughtworks/marsrover/SecondMarsRoverTest.java
+++ src/test/java/com/thoughtworks/marsrover/SecondMarsRoverTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.marsrover;
 
+import static com.thoughtworks.marsrover.second.SecondDirection.EAST;
 import static com.thoughtworks.marsrover.second.SecondDirection.NORTH;
 import static com.thoughtworks.marsrover.second.SecondDirection.WEST;
 import static junit.framework.Assert.assertEquals;
@@ -11,7 +12,7 @@ import org.junit.Test;
 public class SecondMarsRoverTest {
 
   @Test
-  public void return_x6_y6_NORTH_for_receive_move_commander() {
+  public void return_x6_y6_NORTH_for_receive_move_command() {
     SecondMarsRover marsRover = new SecondMarsRover(5, 6, NORTH);
     marsRover.executeCommander(
         (RoverPosition location) ->
@@ -20,11 +21,20 @@ public class SecondMarsRoverTest {
   }
 
   @Test
-  public void return_x5_y6_WEST_receive_turn_left_commander() {
+  public void return_x5_y6_WEST_receive_turn_left_command() {
     SecondMarsRover marsRover = new SecondMarsRover(5, 6, NORTH);
     marsRover.executeCommander(
         (RoverPosition loc) ->
             new RoverPosition(loc.getX(), loc.getY(), loc.getDirection().getNext()));
     assertEquals(marsRover.getRoverPosition(), new RoverPosition(5, 6, WEST));
+  }
+
+  @Test
+  public void return_x5_y6_EAST_for_receive_turn_right_command() {
+    SecondMarsRover marsRover = new SecondMarsRover(5, 6, NORTH);
+    marsRover.executeCommander(
+        (RoverPosition loc) ->
+            new RoverPosition(loc.getX(), loc.getY(), loc.getDirection().getPrevious()));
+    assertEquals(marsRover.getRoverPosition(), new RoverPosition(5, 6, EAST));
   }
 }
